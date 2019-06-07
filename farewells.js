a0224ce12ef8a98e9974fa9c33e4d06daf02d29c
Dianna Sokhan <diannasokhan@gmail.com>
2019-06-07 14:20:59 -0400

    checkpoint work

---
+++ farewells.js
@@ -0,0 +1,15 @@
+exports.farewellEnglish = function(){
+    console.log("Goodbye")
+}
+
+exports.farewellSpanish = function(){
+    console.log("Adiós")
+}
+
+exports.farewellJapanese = function(){
+    console.log("Sayōnara")
+}
+
+exports.farewellIcelandic = function(){
+    console.log("Bless")
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+const greetings = require('./greeting.js');
+const farewells = require('./farewells.js');
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+farewells.farewellEnglish();
+farewells.farewellJapanese();
+farewells.farewellIcelandic();
+farewells.farewellSpanish();
